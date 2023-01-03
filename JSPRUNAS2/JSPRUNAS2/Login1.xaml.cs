cc76324b52518dfca604698837a6c154954fcc5b
User <User@JOHNNY>
2023-01-03 15:18:15 -0500

    Agregar archivos de proyecto.

---
+++ JSPRUNAS2/JSPRUNAS2/Login1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JSPRUNAS2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login1 : ContentPage
+    {
+        public Login1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "jpruna";
+            string contraseña = "12345";
+            if (usuario == txtUsuario.Text && contraseña == txtContraseña.Text)
+            {
+                Navigation.PushAsync(new Login(usuario));
+            }
+            else
+            {
+                DisplayAlert("ALERTA", "Usuario/Contraseña Incorrectos", "CERRAR");
+            }
+        }
+    }
+}
